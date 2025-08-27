be0cfa52110349a44ed2b28db4fb2707f36e8acd
Bivekich <biveki-dev@yandex.ru>
2025-08-27 16:59:19 +0300

    feat(analytics): add Yandex.Metrika with next/script and noscript pixel; silence img lint in noscript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 import { ToastProvider } from "@/components/ui/toast";
 
 const inter = Inter({
@@ -33,6 +34,25 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body className={`${inter.className} antialiased`}>
+        {/* Yandex.Metrika */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103917430', 'ym');
+
+          ym(103917430, 'init', { ssr: true, webvisor: true, clickmap: true, ecommerce: 'dataLayer', accurateTrackBounce: true, trackLinks: true });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="https://mc.yandex.ru/watch/103917430" style={{ position: "absolute", left: -9999 }} alt="" />
+          </div>
+        </noscript>
         <ToastProvider>
           {children}
         </ToastProvider>
